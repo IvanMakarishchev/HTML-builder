32d6e3539fbd58e6786ed8866f7564162302ea13
IvanMakarishchev <denis.flank@gmail.com>
2022-05-19 18:21:36 +0300

    feat: make 02-write-file task

---
+++ 02-write-file/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+const { stdin, stdout } = process;
+
+const filePath = path.join(__dirname, 'text.txt');
+const writeStream = fs.createWriteStream(filePath);
+
+stdout.write('Введите ваш текст (введите exit для выхода):\n');
+stdin.on('data', data => {
+    const input = data.toString();
+    if (input.trim() === 'exit'){
+        stdout.write('Good luck!');
+        process.exit();
+    } else {
+        writeStream.write(input);
+    }
+});
