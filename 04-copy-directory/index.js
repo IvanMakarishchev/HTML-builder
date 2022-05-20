0d85f6fb7cbffef7a0e5a4f670bbaea773e3b4bd
IvanMakarishchev <denis.flank@gmail.com>
2022-05-20 10:17:32 +0300

    feat: make 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const fsPromises = require('fs/promises');
 
+async function makeDirectory() {
+    const newDir = await fsPromises.mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
+}
+
+async function copyFiles() {
+    const readFolder = await fsPromises.readdir(path.join(__dirname, 'files'));
+    for (const file of readFolder) {
+        await fsPromises.copyFile(path.join(__dirname, 'files',  file), path.join(__dirname, 'files-copy', file));
+    }
+}
+
+async function checkFiles() {
+    const readFolder = await fsPromises.readdir(path.join(__dirname, 'files-copy'));
+    for (const file of readFolder) {
+        const compareFiles = await fsPromises.readdir(path.join(__dirname, 'files'));
+        if (!compareFiles.includes(file)) {
+            fs.unlink(path.join(__dirname, 'files-copy', file), (err) => {
+                if (err) { console.log(`Can't delete ${path.join(__dirname, 'files-copy', file)} file`); };
+            });
+        }
+    }
+}
+
+function copyDir () {
+    makeDirectory();
+    copyFiles();
+    checkFiles();
+}
+
+copyDir();
